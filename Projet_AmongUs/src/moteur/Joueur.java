99660109bb4d31537246531ef020dfc6cfc1b114
Ikram RACHAK <46931572+ikramrachak@users.noreply.github.com>
2026-04-27 11:42:41 +0200

    Among_US_IR
    MAJ de classe joueur avec ajout de classe Imposter et classe VJoueur

---
+++ Projet_AmongUs/src/moteur/Joueur.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package moteur;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+/**
+ * @author himiz
+ * Représente un joueur dans le jeu multijoueur.
+ */
+
+public abstract class Joueur {
+    protected int id;
+    protected String nom;
+    protected String motDePasse;
+    protected int scoreTotal;      // score cumulé sur toutes les parties
+    protected int nbFleursTotal;   // nombre total de fleurs cueillies
+    protected int scoreSession;    // score de la partie en cours
+    protected int nbFleursSession; // fleurs cueillies dans la partie en cours
+    protected double posX;
+    protected double posY;
+    protected boolean imposteur;
+
+    // Constructeur principal
+    public Joueur(int id, String nom, String motDePasse) {
+        this.id = id;
+        this.nom = nom;
+        this.motDePasse = motDePasse;
+        this.scoreTotal = 0;
+        this.nbFleursTotal = 0;
+        this.scoreSession = 0;
+        this.nbFleursSession = 0;
+        this.posX = 170;   // position initiale (comme dans Avatar)
+        this.posY = 320;
+        this.imposteur = false;
+    }
+
+    // Constructeur par défaut
+    public Joueur() {
+        this(0, "", "");
+    }
+
+    // --- Getters et Setters ---
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+
+    public String getMotDePasse() { return motDePasse; }
+    public void setMotDePasse(String motDePasse) { this.motDePasse = motDePasse; }
+
+    public int getScoreTotal() { return scoreTotal; }
+    public void setScoreTotal(int scoreTotal) { this.scoreTotal = scoreTotal; }
+
+    public int getNbFleursTotal() { return nbFleursTotal; }
+    public void setNbFleursTotal(int nbFleursTotal) { this.nbFleursTotal = nbFleursTotal; }
+
+    public int getScoreSession() { return scoreSession; }
+    public void setScoreSession(int scoreSession) { this.scoreSession = scoreSession; }
+
+    public int getNbFleursSession() { return nbFleursSession; }
+    public void setNbFleursSession(int nbFleursSession) { this.nbFleursSession = nbFleursSession; }
+
+    public double getPosX() { return posX; }
+    public void setPosX(double posX) { this.posX = posX; }
+
+    public double getPosY() { return posY; }
+    public void setPosY(double posY) { this.posY = posY; }
+
+    public boolean isImposteur() { return imposteur; }
+    public void setImposteur(boolean imposteur) { this.imposteur = imposteur; }
+
+    // --- Méthodes utiles ---
+    //public void cueillirFleur(int points) {
+      //  this.scoreSession += points;
+      //  this.nbFleursSession++;
+    //}
+
+    public void resetPourNouvellePartie() {
+        this.scoreSession = 0;
+        this.nbFleursSession = 0;
+        this.posX = 170;
+        this.posY = 320;
+        this.imposteur = false;
+    }
+
+    public abstract void recolter();
+    
+    @Override
+    public String toString() {
+        return "Joueur{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", scoreTotal=" + scoreTotal +
+                ", scoreSession=" + scoreSession +
+                ", posX=" + posX +
+                ", posY=" + posY +
+                '}';
+    }
+}
---
+++ Projet_AmongUs/src/moteur/VJoueur.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package moteur;
+
+/**
+ *
+ * @author irachak
+ */
+public class VJoueur extends Joueur{
+    public VJoueur(int id, String nom, String motDePasse) {
+        super(id, nom, motDePasse); 
+        this.setImposteur(false);
+    }
+
+    @Override
+    public void recolter() {
+        // L'abeille normale ajoute des points au score
+        this.scoreSession += 10; 
+        System.out.println(this.nom + " a récolté du nectar ! (+10)");
+    }
+    
+}
